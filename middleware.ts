697f2f61f1c7da7408027e7314a657c2902e6f58
DDeluca06 <ddelucalyons@gmail.com>
2025-05-13 11:07:42 -0400

    test. Implicit Any Var Adjusted

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,6 @@ import { NextResponse } from "next/server"
 
 export default withAuth(
   function middleware(req) {
-    // Add custom middleware logic here if needed
     return NextResponse.next()
   },
   {
